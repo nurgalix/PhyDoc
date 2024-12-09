2ce35a5c37811fd8ff9533af75f2107ca4efa50e
Nurgali Nuraly <nurali.nurgali@gmail.com>
2024-12-09 18:17:47 +0500

    Initial Commit

---
+++ PhyDoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhyDoc
+//
+//  Created by Nurgali on 09.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
